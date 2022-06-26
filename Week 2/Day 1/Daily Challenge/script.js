7fa090df850eebbb8fe0db74fed9cb99e89b25d6
Anna <annayampolskey@gmail.com>
2022-06-26 22:04:16 +0300

    dailychallenge

---
+++ Week 2/Day 1/Daily Challenge/script.js
@@ -0,0 +1,26 @@
+//Exercise 1
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0);
+console.log(fruits);
+
+let sortedFruits= fruits.sort();
+console.log(sortedFruits);
+
+sortedFruits.push("Kiwi");
+console.log(sortedFruits);
+
+sortedFruits=["Apples", "Blueberries", "Oranges", "Kiwi"]
+
+let firstItem = sortedFruits[0];
+console.log(firstItem)
+
+console.log(sortedFruits.length) //->4
+console.log(sortedFruits[sortedFruits.length-0]);
+
+let reversedFruits= sortedFruits.reverse();
+console.log(reversedFruits);
+
+//Exercise 2
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+moreFruits[]
+
