25f2e0218e33ec20b7b02db3d567afec6ed6d4e8
Anna <annayampolskey@gmail.com>
2022-06-26 14:46:48 +0300

    java script was added

---
+++ Week 2/Day 1/ExercisesXP/script.js
@@ -0,0 +1,10 @@
+//Exercise1
+
+let favFood= "sushi";
+let favDinner= "lunch";
+let favorite= "I eat"+favFood+"at every"+favDinner;
+console.log(favorite);
+
+//or
+let fav2="I eat"${favFood} "at every" ${favDinner};
+console.log(fav2);
