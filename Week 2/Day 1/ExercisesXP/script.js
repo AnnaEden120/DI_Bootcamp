c8da5c1ad50acd283b3cbb7edab134523180ccda
Anna <annayampolskey@gmail.com>
2022-06-26 15:14:34 +0300

    Exercise2

--- Week 2/Day 1/ExercisesXP/script.js
+++ Week 2/Day 1/ExercisesXP/script.js
@@ -8,3 +8,26 @@ console.log(favorite);
 //or
 let fav2="I eat"${favFood} "at every" ${favDinner};
 console.log(fav2);
+
+//Exercise2
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+console.log(myWatchedSeriesLength);
+console.log(myWatchedSeriesLength[myWatchedSeriesLength-1]);
+let sentence="I watched"$myWatchedSeriesLength "series": $myWatchedSeries
+console.log(myWatchedSeries);
+
+//Exercise2 part 2
+myWatchedSeries.splice(2, "friends")
+console.log(myWatchedSeries);
+
+myWatchedSeries.push("the office");
+console.log(myWatchedSeries);
+
+myWatchedSeries.unshift("Gilmore Girls");
+console.log(myWatchedSeries);
+
+myWatchedSeries.splice(1);
+console.log(myWatchedSeries)
+
+
+
