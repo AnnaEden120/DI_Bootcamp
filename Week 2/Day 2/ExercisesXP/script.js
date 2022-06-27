989f9857cb6d003b5e5798f805266d6ce5c8cd4b
Anna <annayampolskey@gmail.com>
2022-06-27 15:21:06 +0300

    exercise xp

---
+++ Week 2/Day 2/ExercisesXP/script.js
@@ -0,0 +1,32 @@
+//Exercise 1
+let x = 5;
+let y = 2;
+
+You should display:
+x is the biggest number
+
+if (x==5){
+	console.log("x is the biggest number")
+} else {
+	console.log("y is the smallest number")
+}
+
+
+//Exercise 2
+//Instructions
+//Create a variable called newDog where it’s value is “Chihuahua”.
+
+let newDog="Chihuahua";
+
+//Check and display how many letters are in newDog.
+let newDog= text;
+let length= text.length;
+console.log(length);
+
+//Display the newDog variable in uppercase and then in lowercase (no need to create new variables, just console.log twice).
+let upperCase=text.toUpperCase();
+Let lowerCase=text.toLowerCase();
+
+//Check if the variable newDog is equal to “Chihuahua”
+//if it does, display ‘I love Chihuahuas, it’s my favorite dog breed’
+//else, console.log ‘I dont care, I prefer cats’
