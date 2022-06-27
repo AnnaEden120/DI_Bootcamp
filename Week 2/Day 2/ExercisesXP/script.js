6a03e3000b256ee90097cb89d9ccc8e659c7caba
Anna <annayampolskey@gmail.com>
2022-06-27 16:15:33 +0300

    done exercise

--- Week 2/Day 2/ExercisesXP/script.js
+++ Week 2/Day 2/ExercisesXP/script.js
@@ -30,3 +30,61 @@ Let lowerCase=text.toLowerCase();
 //Check if the variable newDog is equal to “Chihuahua”
 //if it does, display ‘I love Chihuahuas, it’s my favorite dog breed’
 //else, console.log ‘I dont care, I prefer cats’
+
+if (newDog="Chihuahua"){
+	console.log("I love Chihuahuas, it’s my favorite dog breed!")
+} else {
+	console.log("I dont care, I prefer cats");
+
+//Exercise 3
+//Instructions
+//Prompt the user for a number and save it to a variable.
+const number = prompt("Enter a number: ");
+
+//Check whether the variable is even or odd.
+//If it is even, display: “x is an even number”. Where x is the actual number the user chose.
+//If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+
+
+if(number % 2 == 0) {
+	console.log("The number is even.");
+}
+
+else {
+	console.log("The number is odd.");
+}
+
+let number="x";
+
+if(number % 2 == 0) {
+	console.log("[number] is an even number");
+}
+
+else {
+	console.log("[number] is an odd number");
+}
+
+
+//Exercise 4
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+let numberUsers=user.length;
+let userNumber= users.length;
+console.log(userNumber);
+4
+
+
+if (userNumber==0){
+	console.log("no one is online")
+} 
+
+if (userNumber==1){
+	console.log(“<name_user> is online”)
+}
+
+if (userNumber==2){
+	console.log(“<name_user1> and <name_user2> are online”)
+}
+
+if (userNumber>2){
+	console.log(users[0,1]+"and 3 more are online")
+}
