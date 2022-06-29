d70c59a2ad7ee40387113b1da13fec57714ea35d
Anna <annayampolskey@gmail.com>
2022-06-29 23:16:55 +0300

    dailychallenge

---
+++ Week 2/Day 4/Daily Challenge/script.js
@@ -0,0 +1,12 @@
+prompt("Write something");
+let userAnswer= [a, b, c, d];
+let word1= userAnswer[a];
+let word2= userAnswer[b];
+let word3= userAnswer[c];
+let word4= userAnswer[d];
+let str='*';
+
+function rectangularFrame(){
+//got stuck here...	
+
+
