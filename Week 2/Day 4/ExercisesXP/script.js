ea1081c88f734ec985fcab0f6a3649352ae7a4d2
Anna <annayampolskey@gmail.com>
2022-06-29 17:36:44 +0300

    exerciseXP

---
+++ Week 2/Day 4/ExercisesXP/script.js
@@ -0,0 +1,30 @@
+//Exercise 1
+//Part I : function with no parameters
+
+//Create a function called infoAboutMe() that takes no parameter.
+function aboutMe(){
+	console.log("hello")
+}
+//The function should console.log a sentence about you (ie. your name, age, hobbies ect…).
+//Call the function.
+function aboutMe(name,age){
+	console.log("My name is" $name "my age is" $age)
+}
+
+aboutMe("Anna", 32);
+//Part II : function with three parameters
+
+//Create a function called infoAboutPerson(personName, personAge, personFavoriteColor) that takes 3 parameters.
+//The function should console.log a sentence about the person (ie. “You name is …, you are .. years old, your favorite color is …”)
+//Call the function twice with the following arguments:
+//infoAboutPerson("David", 45, "blue")
+//infoAboutPerson("Josh", 12, "yellow")
+
+function inforAboutPerson(personName, personAge, personFavoriteColor){
+	console.log("Your name is"  +personName+  "you are"  +personAge+ "years old and your favorite color is" +personFavoriteColor)
+}
+
+//Your name is David you are 45 years old and your favorite color is blue
+//Your name is 	Josh you are 12 years old and your favorite color is yellow
+
+
