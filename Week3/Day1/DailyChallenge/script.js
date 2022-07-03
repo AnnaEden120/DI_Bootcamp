2ffb42e8e35e75725cbac510f2fc1892cd9d39a1
Anna <annayampolskey@gmail.com>
2022-07-03 23:01:31 +0300

    dailychallenge

---
+++ Week3/Day1/DailyChallenge/script.js
@@ -0,0 +1,18 @@
+//Create an array which value is the planets of the solar system.
+let listPlanets=["earth","moon","venus","Mercury","Satrun","Jupiter","Mars", "Pluto","Neptune","Uranus"];
+//For each planet in the array, create a <div> using createElement. This div should have a class named "planet".
+listPlanets=createElement(div class="planet");
+//Each planet should have a different background color. (Hint: you could add a new class to each planet - each class containing a different background-color).
+Venus.style.background-color="orange";
+Mercury.style.background-color="blue";
+Saturn.style.background-color="white";
+Jupiter.style.background-color="brown";
+Mars.style.background-color="red";
+Pluto.style.background-color="brown";
+Neptune.style.background-color="blue";
+Uranus.style.background-color="blue";
+//Finally append each div to the <section> in the HTML (presented below).
+document.appendChild(listPlanets)=(listPlanets(planet));
+//Bonus: Do the same process to create the moons.
+//Be careful, each planet has a certain amount of moons. How should you display them?
+//Should you still use an array for the planets ? Or an array of objects ?
