fe579e8827520d5b9fbb20f46947a21c06340630
Anna <annayampolskey@gmail.com>
2022-07-04 22:09:23 +0300

    exercisexp

--- Week3/Day2/ExercisesXP/script.js
+++ Week3/Day2/ExercisesXP/script.js
@@ -11,9 +11,20 @@ redClick.addEventListener("click",function(){
 	redClick.style.backgroundcolor="red";
 }
 //Add an event listener which will hide the h3 when it’s clicked on (use the display:none property).
-
+var history=document.getElementsByTagName("h3"[0]);
+history.addEventListener("click",function(){ 
+	history.style.display="none";
+}
 //Add a <button> to the HTML file, that when clicked on, should make the text of all the paragraphs, bold.
-
+var clickBold = document.getElementsByTagName("button");
+button.addEventListener("click", function{
+	p.style.font-weight="bold";
+}
 //BONUS : When you hover on the h1, set the font size to a random pixel size between 0 to 100.(Check out this documentation)
-
+//html-<h1 id="some" onmouseover="someFacts"> Some Facts </h1>
+function (){
+	someFacts.style.fontSize=Math.floor(Math.random()*100)+"px";
+}
 //BONUS : When you hover on the 2nd paragraph, it should fade out (Check out “fade css animation” on Google)
+//<p id="after" onmouseover="popular">
+let x = getElementsByID="after";
