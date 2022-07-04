0678cdfb8dbc7e0db7f60523c5a941dc3850fcd5
Anna <annayampolskey@gmail.com>
2022-07-04 23:50:08 +0300

    exercisexp3

--- Week3/Day2/ExercisesXP/script.js
+++ Week3/Day2/ExercisesXP/script.js
@@ -26,5 +26,56 @@ function (){
 	someFacts.style.fontSize=Math.floor(Math.random()*100)+"px";
 }
 //BONUS : When you hover on the 2nd paragraph, it should fade out (Check out “fade css animation” on Google)
-//<p id="after" onmouseover="popular">
-let x = getElementsByID="after";
+//in html:
+//<style>
+//.hoverOut {
+	//position: relative;
+	//-webkit-transition: all 1s ease-in-out;
+	//-moz-transition: all 1s ease-in-out;
+	//-ms-transition: all 1s ease-in-out;
+	//-o-transition: all 1s ease-in-out;
+	//transition: all 1s ease-in-out;
+//	text-align:center;
+//	font-size:0;
+
+//}
+//.hoverOut:hover{
+//	font-size:14px;
+	//color:white;
+
+//}
+//</style>
+//Exercise2- Ziv told us not to do it because we did not learn forms yet.
+
+//Exercise3
+//Declare a global variable named allBoldItems.
+var allBoldItems
+//Create a function called getBold_items() that takes no parameter. This function should collect all the bold items inside the paragraph,
+ //and assign them to the allBoldItems variable.
+ function getBold_items(){
+ 	let allBoldItems=document.querySelectorAll("strong").innerHTML;
+ }
+//Create a function called highlight() that changes the color of all the bold text to blue.
+function highlight(){
+	allBoldItems.style.color="blue";
+}
+//Create a function called return_normal() that changes the color of all the bold text back to black.
+function return_normal(){
+	allBoldItems.style.color="black";
+}
+//Call the function highlight() onmouseover (ie. when the mouse pointer is moved onto the paragraph), and the function return_normal() onmouseout (ie. when the mouse pointer is moved out of the paragraph). Look at this example
+//in html
+//<p onmouseover="hightlight(this)" onmouseout="return_noraml(this)">.......</p>
+function highlight(x) {
+	x.style.height = "64px";
+	x.style.width = "64px";
+}
+
+function return_normal(x) {
+	x.style.height = "32px";
+	x.style.width = "32px";
+}
+
+
+
+
