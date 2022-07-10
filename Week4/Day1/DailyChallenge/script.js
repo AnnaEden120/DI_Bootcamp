8aeeaef22e212526088b29c0a1ded6a175adb9d9
Anna <annayampolskey@gmail.com>
2022-07-11 00:13:50 +0300

    dailychallenge

---
+++ Week4/Day1/DailyChallenge/script.js
@@ -0,0 +1,48 @@
+const gameInfo = [
+{
+ username: "john",
+ team: "red",
+ score: 5,
+ items: ["ball", "book", "pen"]
+},
+{
+ username: "becky",
+ team: "blue",
+ score: 10,
+ items: ["tape", "backpack", "pen"]
+},
+{
+ username: "susy",
+ team: "red",
+ score: 55,
+ items: ["ball", "eraser", "pen"]
+},
+{
+ username: "tyson",
+ team: "green",
+ score: 1,
+ items: ["book", "pen"]
+},
+];
+
+//Create an array using forEach that contains all the usernames from the gameInfo array, add an exclamation point (ie. “!”) to the end of every username.
+//The new array should look like this :
+let usernames = ["john!", "becky!", "susy!", "tyson!"]
+
+gameInfo.forEach(username[0]){
+  let usernames=console.log('username[0]'+"!")
+}
+
+
+//2. Create an array using forEach that contains the usernames of all players with a score bigger than 5. Use the ternary operator
+//The new array should look like this :
+
+let winners = ["becky", "susy"]
+let gameInfo(score)= x;
+let winners=console.log(x<5? username)
+
+
+//3. Find and display the total score of the users. (Hint: The total score is 71)
+gameInfo.foreach(score[0]){
+  let totalScore=console.log(score[sum])
+}
