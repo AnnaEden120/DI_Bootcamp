1fc99247d18581f529201f251f8aaeffea73d996
Anna <annayampolskey@gmail.com>
2022-07-10 23:54:53 +0300

    exercisexp

--- Week4/Day1/ExercisesXP/script.js
+++ Week4/Day1/ExercisesXP/script.js
@@ -131,7 +131,13 @@ colors.forEach(function(item){
 	}
 }
 }
+//Exercise5
+let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+let ordinal = ["th","st","nd","rd"];
 
+//Write a JavaScript program that displays the colors in the following order :
+ //“1st choice is Blue .” “2nd choice is Green.” “3rd choice is Red.” ect…
+//Hint : Use the array methods taught in class and ternary operator.
 
-
-
+let colorOrdinal
+colorOrdinal.forEach(color, ordinal)=>ordinal[index]+"choice is"+color[index];
