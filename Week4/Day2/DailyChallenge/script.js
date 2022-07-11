66ba1ddc70898dda73cf22503d60976006417576
Anna <annayampolskey@gmail.com>
2022-07-11 23:33:32 +0300

    dailychallenge

---
+++ Week4/Day2/DailyChallenge/script.js
@@ -0,0 +1,25 @@
+let client = "John";
+
+let groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+//1.
+const displayGroceries= fruits;
+displayGroceries.forEach(element=>console.log(element));
+//2.
+const cloneGroceries;
+var user={value:$client};
+client.value="Betty"
+consloe.log(client);//betty
+//We will see the change in the user becuase "client" and "user" are the same.
+var shopping;
+consloe.log(shopping==displayGroceries);
+totalPrice.value="35$";
+payed.value="false";
