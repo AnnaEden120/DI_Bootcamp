b95753f06304e5166dd195596318ba76bf1d05f4
Anna <annayampolskey@gmail.com>
2022-07-13 12:06:15 +0300

    exercisexp

---
+++ Week4/Day4/ExercisesXP/script.js
@@ -0,0 +1,22 @@
+//Exercise1
+const person = {
+	name: 'John Doe',
+	age: 25,
+	location: {
+		country: 'Canada',
+		city: 'Vancouver',
+		coordinates: [49.2827, -123.1207]
+	}
+}
+
+const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+
+console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+//I am John Doe from Vancouver, Canda. Latitude (49.2827), Longitude(-123.1207)
+
+//Exercise2
+function displayStudentInfo(objUser){
+    //destructuring
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
