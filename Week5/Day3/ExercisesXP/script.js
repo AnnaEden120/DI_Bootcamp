5cf4266b780116d38a66bf0c1507384f95413b60
Anna <annayampolskey@gmail.com>
2022-07-19 13:24:27 +0300

    exercisexp

---
+++ Week5/Day3/ExercisesXP/script.js
@@ -0,0 +1,26 @@
+//Exercise1
+//1+2.
+let compare=function (compareToTen(num);
+
+	let options = new Promise(compareToTen(resolve, reject) {
+		if (num < 10) {
+			resolve("Ok");
+		} else if (num>10) {
+			resolve("Not ok");
+		} else {
+			reject("Not ok at all");
+		}
+	});
+
+
+	options.then(compareToTen(num){
+		console.log("This is", JSON.string(value));
+	}
+
+	options.catch(compareToTen(num){
+		consoloe.log("Oh no", value);
+	}
+
+	options.finally(compareToTen(num){
+		consoloe.log("Sorry,", value);
+	}
