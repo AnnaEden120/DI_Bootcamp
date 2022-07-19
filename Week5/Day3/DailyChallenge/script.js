d91dcb86c4f403b1d17d22b467eb06474cb92f5a
Anna <annayampolskey@gmail.com>
2022-07-19 21:13:16 +0300

    dailychallenge1

---
+++ Week5/Day3/DailyChallenge/script.js
@@ -0,0 +1,27 @@
+//first
+//1.
+let arr=(["word1", "word2", "word3"]);
+
+function makeAllCaps(arr) {
+	const promise= new Promise((resolve, reject){
+		if (arr==string) {
+			resolve(arr.toUpperCase());     
+		} else {
+			promise.reject("Not good")
+		}
+	});
+}
+
+//2.
+const arr2=arr.toUpperCase();
+
+function sortWords(arr2){
+	const promise2= new Promise((resolve, reject){
+		if (arr2.length>4){
+			resolve(arr2.sort());
+		} else{
+			promise2.reject("No")
+		}
+	});
+}
+
