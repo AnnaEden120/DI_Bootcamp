a9bf21ea6aba719ed5fb2db70b260fcb13cbf3a1
Anna <annayampolskey@gmail.com>
2022-07-20 22:04:32 +0300

    dailychallenge

---
+++ Week5/Day4/DailyChallenge/script.js
@@ -0,0 +1,10 @@
+//first
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+	setTimeout(resolve, 3000, 'foo');
+});
+
+function Promise.all([promise1, promise2, promise3]).then((values).catch(error)=>
+	console.log(values, error);
